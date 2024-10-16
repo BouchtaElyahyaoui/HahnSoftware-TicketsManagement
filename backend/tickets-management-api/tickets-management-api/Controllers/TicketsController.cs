b09c2447b465b6de706df665faf67e5b04e84ed2
BouchtaElyahyaoui <bouchtaelyahyaoui2264@gmail.com>
2024-10-16 21:55:20 +0200

    Backned - Add ticket controller

---
+++ backend/tickets-management-api/tickets-management-api/Controllers/TicketsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using tickets_management_api.services;
+
+namespace tickets_management_api.Controllers
+{
+    public class TicketsController : ControllerBase
+    {
+        private readonly ITicketService _service;
+
+        public TicketsController(ITicketService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllTickets()
+        {
+            var tickets = await _service.GetAllTickets();
+            return Ok(tickets);
+        }
+    }
+}
