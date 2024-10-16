157701e7c5e178875607723f8318b189a91a550f
BouchtaElyahyaoui <bouchtaelyahyaoui2264@gmail.com>
2024-10-16 21:24:14 +0200

    Add .gitignore file && Add ticket model

---
+++ backend/tickets-management-api/tickets-management-api/Models/Ticket.cs
@@ -0,0 +1,10 @@
+﻿namespace tickets_management_api.Models
+{
+    public class Ticket
+    {
+        public int Id { get; set; }
+        public required string Description { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+
+}
