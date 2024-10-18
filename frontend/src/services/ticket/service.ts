9802b598fac4145760a2d8db6bec167475867378
BouchtaElyahyaoui <bouchtaelyahyaoui2264@gmail.com>
2024-10-18 20:46:51 +0200

    Frontend - add get all tickets service

--- frontend/src/services/ticket/service.ts
+++ frontend/src/services/ticket/service.ts
@@ -1,1 +1,11 @@
 import axios from 'axios';
+
+
+const API_URL = 'http://localhost:5000/api/tickets';
+
+
+export const getTickets = async () => {
+  const response = await axios.get(API_URL);
+  console.log("Response",response);
+  return response.data;
+};
